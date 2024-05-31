1375703f6c3f04b816cffad0a5c2ff97c6f0b1d3
devfabiorm <fabio.evangelion.guitar@gmail.com>
2024-05-30 23:41:17 -0300

    Created endpoint and use case to return all pets

--- Petfolio/Controllers/PetController.cs
+++ Petfolio/Controllers/PetController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Petfolio.Application.UseCases.Pets.GetAll;
 using Petfolio.Application.UseCases.Pets.Register;
 using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
@@ -31,6 +32,23 @@ public class PetController : ControllerBase
         var useCase = new UpdatePetUseCase();
 
         useCase.Execute(id, request);
+
+        return NoContent();
+    }
+
+    [HttpGet]
+    [ProducesResponseType(typeof(ResponseAllPetJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public IActionResult GetAll()
+    {
+        var useCase = new GetAllPetUseCase();
+
+        var response = useCase.Execute();
+
+        if(response.Pets.Count == 0)
+        {
+            return Ok(response);
+        }
 
         return NoContent();
     }
